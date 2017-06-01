c4eca47f12fedaecafc014a6d833b18e9a5808e8
hors-lignes <compte@hors-lignes.com>
2017-06-01 10:28:55 +0200

    désactivation des emojis wordpress

--- theme/functions.php
+++ theme/functions.php
@@ -2,9 +2,17 @@
 
 define( 'TPL_DIR', get_template_directory_uri() );
 
+// DISABLE EMOJIS 
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+add_filter( 'emoji_svg_url', '__return_false' );
+// /DISABLE EMOJIS
+
+// LOAD ASSETS
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'vendor', TPL_DIR.'/vendor.css' );
 	wp_enqueue_style( 'theme', TPL_DIR.'/theme.css' );
 	wp_enqueue_script( 'vendor', TPL_DIR.'/vendor.js' );
 	wp_enqueue_script( 'theme', TPL_DIR.'/theme.js' );
 } );
+// /LOAD ASSETS
