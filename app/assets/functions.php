18c49c7740970da650356500d5b89508ede4987e
hors-lignes <compte@hors-lignes.com>
2017-06-07 09:23:21 +0200

    ajout des miniatures sur les posts et des menus

--- app/assets/functions.php
+++ app/assets/functions.php
@@ -10,9 +10,16 @@ add_filter( 'emoji_svg_url', '__return_false' );
 
 // LOAD ASSETS
 add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_style( 'vendor', TPL_DIR.'/vendor.css' );
-	wp_enqueue_style( 'theme', TPL_DIR.'/theme.css' );
-	wp_enqueue_script( 'vendor', TPL_DIR.'/vendor.js' );
-	wp_enqueue_script( 'theme', TPL_DIR.'/theme.js' );
+    wp_enqueue_style( 'vendor', TPL_DIR.'/vendor.css' );
+    wp_enqueue_style( 'theme', TPL_DIR.'/theme.css' );
+    wp_enqueue_script( 'vendor', TPL_DIR.'/vendor.js' );
+    wp_enqueue_script( 'theme', TPL_DIR.'/theme.js' );
 } );
 // /LOAD ASSETS
+
+// THEME SETUP
+add_action( 'after_setup_theme', function() {
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'menus' );
+} );
+// / THEME SETUP
